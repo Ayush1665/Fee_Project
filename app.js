dc22fca5652ac2eb3271527fccd6a8ee6796a3eb
Ayush1665 <101383376+Ayush1665@users.noreply.github.com>
2023-09-12 09:44:13 +0530

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,109 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
 
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Samsung Galaxy S21 5G 128 GB',
+        image: '1.webp',
+        price: 74999
+    },
+    {
+        id: 2,
+        name: 'Samsung Galaxy M33 5G 128 GB',
+        image: '2.webp',
+        price: 20499
+    },
+    {
+        id: 3,
+        name: 'vivo Y56 5G Orange  128 GB',
+        image: '3.webp',
+        price: 17999
+    },
+    {
+        id: 4,
+        name: 'vivo T2x 5G Aurora Gold 128 GB',
+        image: '4.webp',
+        price: 20999
+    },
+    {
+        id: 5,
+        name: 'Mi 11X Celestial Silver 128 GB ',
+        image: '5.jpeg',
+        price: 24999
+    },
+    {
+        id: 6,
+        name: 'Redmi 12 5G 256 GB Silver',
+        image: '6.jpeg',
+        price: 19999
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Cart</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+    
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
